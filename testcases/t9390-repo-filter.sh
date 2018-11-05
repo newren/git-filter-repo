ad59fffed0b6fbec87d5d4cc9d15bbafe30d8d48
Elijah Newren <newren@gmail.com>
2018-11-05 06:30:50 -0800

    Switchover to git test-lib.sh style testing

---
+++ testcases/t9390-repo-filter.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+test_description='repo-filter'
+
+. ./test-lib.sh
+
+#set -x
+
+DATA="$TEST_DIRECTORY/t9390"
+
+filter_testcase() {
+	INPUT=$1
+	OUTPUT=$2
+	shift 2
+	REST=("$@")
+
+
+	NAME="check: $INPUT -> $OUTPUT using '${REST[@]}'"
+	test_expect_success "$NAME" '
+		# Clean up from previous run
+		git pack-refs --all &&
+		rm .git/packed-refs &&
+
+		# Run the example
+		cat $DATA/$INPUT | ../../git-fast-filter --stdin --quiet --force ${REST[@]} &&
+
+		# Compare the resulting repo to expected value
+		git fast-export --use-done-feature --all >compare &&
+		test_cmp $DATA/$OUTPUT compare
+	'
+}
+
+filter_testcase case1 case1-filename --path filename
+filter_testcase case1 case1-twenty   --path twenty
+filter_testcase case1 case1-ten      --path ten
+
+test_done
