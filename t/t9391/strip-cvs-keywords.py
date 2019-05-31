7d42c2093cd4e6690dda5e9c9a1139d4be9ab69b
Elijah Newren <newren@gmail.com>
2019-05-31 13:31:02 -0700

    filter-repo: limit splicing repos warning to test that splices repos
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- t/t9391/strip-cvs-keywords.py
+++ t/t9391/strip-cvs-keywords.py
@@ -4,11 +4,6 @@
 Please see the
   ***** API BACKWARD COMPATIBILITY CAVEAT *****
 near the top of git-filter-repo.
-
-Also, note that splicing repos may need some special care as fast-export
-only shows the files that changed relative to the first parent, so there
-may be gotchas if you are to splice near merge commits; this example does
-not try to handle any such special cases.
 """
 
 import re
