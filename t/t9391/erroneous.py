e4960a53f852a9accde3ff6599f2f4c61c3d189d
Elijah Newren <newren@gmail.com>
2020-07-27 09:49:43 -0700

    Fix undefined variable names
    Reported-by: Christian Clauss <cclauss@me.com>
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- t/t9391/erroneous.py
+++ t/t9391/erroneous.py
@@ -9,7 +9,7 @@ near the top of git-filter-repo
 import git_filter_repo as fr
 
 def handle_tag(tag):
-  print("Decipher this: "+''.join(reversed(progress.message)))
+  print("Tagger: "+''.join(tag.tagger_name))
 
 args = fr.FilteringOptions.parse_args(['--force', '--tag-callback', 'pass'])
 filter = fr.RepoFilter(args, tag_callback = handle_tag)
