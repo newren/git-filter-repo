c35b573499b75bfe9c3061900ffe160732902be5
James Morris <jamesmorris2@gmail.com>
2021-10-26 22:14:28 -0400

    Remove unneeded setup.py code

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
 from setuptools import setup
-import os
-
-
-def link_parent(src, target=None):
-    if target is None:
-        target = src
-    try:
-        os.symlink(os.path.join("..", src), target)
-    except FileExistsError:
-        pass
-
-
-for f in ['git-filter-repo', 'README.md']:
-    link_parent(f)
-
-link_parent('git-filter-repo', 'git_filter_repo.py')
-
 
 setup(use_scm_version=dict(root="..", relative_to=__file__))
