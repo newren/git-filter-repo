63d30d7772bda4a9c0631aad4a9b56d8d122e572
James Morris <jamesmorris2@gmail.com>
2021-10-26 22:10:28 -0400

    Specify entry points in setup.cfg

--- setup.py
+++ setup.py
@@ -17,5 +17,4 @@ for f in ['git-filter-repo', 'README.md']:
 link_parent('git-filter-repo', 'git_filter_repo.py')
 
 
-setup(use_scm_version=dict(root="..", relative_to=__file__),
-      entry_points={'console_scripts': ['git-filter-repo = git_filter_repo:main']})
+setup(use_scm_version=dict(root="..", relative_to=__file__))
