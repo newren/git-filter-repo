a59e67e7918e577147ca36a70916741be029c878
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-12 14:19:31 -0700

    fix duplicate setuptools entry point script
    Signed-off-by: Theodore Ni <3806110+tjni@users.noreply.github.com>

--- release/setup.py
+++ release/setup.py
@@ -11,11 +11,9 @@ def link_parent(src, target=None):
         pass
 
 
-for f in ['git-filter-repo', 'README.md']:
-    link_parent(f)
+link_parent('README.md')
 
 link_parent('git-filter-repo', 'git_filter_repo.py')
 
 
-setup(use_scm_version=dict(root="..", relative_to=__file__),
-      entry_points={'console_scripts': ['git-filter-repo = git_filter_repo:main']})
+setup(use_scm_version=dict(root="..", relative_to=__file__))
